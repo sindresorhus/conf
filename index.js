5e68e0a61c0eddec11454ce5620fd00134c6b05d
Sindre Sorhus <sindresorhus@gmail.com>
2017-04-25 17:26:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const obj = () => Object.create(null);
 // Prevent caching of this module so module.parent is always accurate
 delete require.cache[__filename];
 const parentDir = path.dirname(module.parent.filename || '.');
+
 class Conf {
 	constructor(opts) {
 		const pkgPath = pkgUp.sync(parentDir);
