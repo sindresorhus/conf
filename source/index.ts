0b0a98da29579b5a6758e2ad0206fc24758cd7d3
Rafael Ramalho <rafazelramalho19@gmail.com>
2019-11-25 10:46:22 +0000

    fix: add docs and refactor types

--- source/index.ts
+++ source/index.ts
@@ -37,67 +37,7 @@ const checkValueType = (key: string, value: unknown): void => {
 const INTERNAL_KEY = '__internal__';
 const MIGRATION_KEY = `${INTERNAL_KEY}.migrations.version`;
 
-export type Serializer = (...args: unknown[]) => string;
-export type Deserializer = (arg: string | Buffer) => unknown;
-export type DefaultValues = {
-	[key: string]: object;
-};
-export type Migrations = {
-	[key: string]: (store: Conf) => void;
-};
-
-/**
-[JSON Schema](https://json-schema.org) to validate your config data.
-Under the hood, the JSON Schema validator [ajv](https://github.com/epoberezkin/ajv) is used to validate your config. We use [JSON Schema draft-07](http://json-schema.org/latest/json-schema-validation.html) and support all [validation keywords](https://github.com/epoberezkin/ajv/blob/master/KEYWORDS.md) and [formats](https://github.com/epoberezkin/ajv#formats)
-You should define your schema as an object where each key is the name of your data's property and each value is a JSON schema used to validate that property. See more [here](https://json-schema.org/understanding-json-schema/reference/object.html#properties)
-@example
-```
-import Conf = require('conf');
-const schema = {
-    foo: {
-		type: 'number',
-		maximum: 100,
-		minimum: 1,
-		default: 50
-	},
-	bar: {
-		type: 'string',
-		format: 'url'
-	}
-};
-const config = new Conf({schema});
-console.log(config.get('foo'));
-//=> 50
-config.set('foo', '1');
-// [Error: Config schema violation: `foo` should be number]
-```
-**Note:** The `default` value will be overwritten by the `defaults` option if set.
-*/
-
-export type Schema = object | boolean;
-
-export type Options = {
-	accessPropertiesByDotNotation?: boolean;
-	clearInvalidConfig?: boolean;
-	configName?: string;
-	cwd?: string;
-	defaults?: object;
-	encryptionKey?: string;
-	fileExtension?: string;
-	migrations?: Migrations;
-	projectName?: string;
-	projectSuffix?: string;
-	projectVersion?: string;
-	/**
-	 * @type Schema
-	 */
-	schema?: Schema;
-	watch?: boolean;
-	serialize: Serializer;
-	deserialize?: Deserializer;
-};
-
-export default class Conf {
+export default class Conf<T = any> implements Iterable<[string, T]> {
 	_options: Options;
 
 	_defaultValues: DefaultValues = {};
@@ -114,6 +54,9 @@ export default class Conf {
 
 	path: string;
 
+	/**
+	Simple config handling for your app or module.
+	*/
 	constructor(partialOptions?: Partial<Options>) {
 		const options: Options = {
 			configName: 'config',
@@ -193,7 +136,7 @@ export default class Conf {
 		const store = Object.assign(plainObject(), options.defaults, fileStore);
 		this._validate(store);
 		try {
-			assert.deepStrictEqual(fileStore, store);
+			assert.deepEqual(fileStore, store);
 		} catch (_) {
 			this.store = store;
 		}
@@ -362,6 +305,12 @@ export default class Conf {
 		this.store = store;
 	}
 
+	/**
+	Get an item.
+
+	@param key - The key of the item to get.
+	@param defaultValue - The default value if the item does not exist.
+	*/
 	get(key: string, defaultValue?: unknown): unknown {
 		if (this._options.accessPropertiesByDotNotation) {
 			return dotProp.get(this.store, key, defaultValue);
@@ -370,6 +319,12 @@ export default class Conf {
 		return key in this.store ? this.store[key] : defaultValue;
 	}
 
+	/**
+	Set an item.
+
+	@param key - You can use [dot-notation](https://github.com/sindresorhus/dot-prop) in a key to access nested properties.
+	@param value - Must be JSON serializable. Trying to set the type `undefined`, `function`, or `symbol` will result in a `TypeError`.
+	*/
 	set(key: string | object, value?: unknown): void {
 		if (typeof key !== 'string' && typeof key !== 'object') {
 			throw new TypeError(`Expected \`key\` to be of type \`string\` or \`object\`, got ${typeof key}`);
@@ -406,6 +361,11 @@ export default class Conf {
 		this.store = store;
 	}
 
+	/**
+	Check if an item exists.
+
+	@param key - The key of the item to check.
+	*/
 	has(key: string): boolean {
 		if (this._options.accessPropertiesByDotNotation) {
 			return dotProp.has(this.store, key);
@@ -414,6 +374,11 @@ export default class Conf {
 		return key in this.store;
 	}
 
+	/**
+	Reset items to their default values, as defined by the `defaults` or `schema` option.
+
+	@param keys - The keys of the items to reset.
+	*/
 	reset(...keys: string[]): void {
 		for (const key of keys) {
 			if (this._defaultValues[key]) {
@@ -422,6 +387,11 @@ export default class Conf {
 		}
 	}
 
+	/**
+	Delete an item.
+
+	@param key - The key of the item to delete.
+	*/
 	delete(key: string): void {
 		const {store} = this;
 		if (this._options.accessPropertiesByDotNotation) {
@@ -433,10 +403,19 @@ export default class Conf {
 		this.store = store;
 	}
 
+	/**
+	Delete all items.
+	*/
 	clear(): void{
 		this.store = plainObject();
 	}
 
+	/**
+	Watches the given `key`, calling `callback` on any changes.
+
+	@param key - The key wo watch.
+	@param callback - A callback function that is called on any changes. When a `key` is first set `oldValue` will be `undefined`, and when a key is deleted `newValue` will be `undefined`.
+	*/
 	onDidChange(key: string, callback: (...args: unknown[]) => void): () => unknown {
 		if (typeof key !== 'string') {
 			throw new TypeError(`Expected \`key\` to be of type \`string\`, got ${typeof key}`);
@@ -451,6 +430,11 @@ export default class Conf {
 		return this.handleChange(getter, callback);
 	}
 
+	/**
+	Watches the whole config object, calling `callback` on any changes.
+
+	@param callback - A callback function that is called on any changes. When a `key` is first set `oldValue` will be `undefined`, and when a key is deleted `newValue` will be `undefined`.
+	*/
 	onDidAnyChange(callback: () => unknown): () => unknown {
 		if (typeof callback !== 'function') {
 			throw new TypeError(`Expected \`callback\` to be of type \`function\`, got ${typeof callback}`);
@@ -469,8 +453,8 @@ export default class Conf {
 			const newValue = getter();
 
 			try {
-				// TODO: Use `util.isDeepStrictEqual` when targeting Node.js 10
-				assert.deepStrictEqual(newValue, oldValue);
+				// TODO: Use `util.isdeepEqual` when targeting Node.js 10
+				assert.deepEqual(newValue, oldValue);
 			} catch (_) {
 				currentValue = newValue;
 				callback.call(this, newValue, oldValue);
@@ -539,9 +523,224 @@ export default class Conf {
 		this.events?.emit('change');
 	}
 
-	* [Symbol.iterator](): unknown {
+	* [Symbol.iterator](): IterableIterator<[string, any]> {
 		for (const [key, value] of Object.entries(this.store)) {
 			yield [key, value];
 		}
 	}
 }
+
+export type Serializer = (...args: unknown[]) => string;
+export type Deserializer = (arg: string | Buffer) => unknown;
+export type DefaultValues = {
+	[key: string]: object;
+};
+export type Migrations = {
+	[key: string]: (store: Conf) => void;
+};
+
+export type Schema = object | boolean;
+
+export type Options = {
+	/**
+		Access nested properties by dot notation.
+
+		@default true
+
+		@example
+		```
+		const config = new Conf();
+
+		config.set({
+			foo: {
+				bar: {
+					foobar: '🦄'
+				}
+			}
+		});
+
+		console.log(config.get('foo.bar.foobar'));
+		//=> '🦄'
+		```
+
+		Alternatively, you can set this option to `false` so the whole string would be treated as one key.
+
+		@example
+		```
+		const config = new Conf({accessPropertiesByDotNotation: false});
+
+		config.set({
+			`foo.bar.foobar`: '🦄'
+		});
+
+		console.log(config.get('foo.bar.foobar'));
+		//=> '🦄'
+		```
+
+	*/
+	accessPropertiesByDotNotation?: boolean;
+
+	/**
+		The config is cleared if reading the config file causes a `SyntaxError`. This is a good default, as the config file is not intended to be hand-edited, so it usually means the config is corrupt and there's nothing the user can do about it anyway. However, if you let the user edit the config file directly, mistakes might happen and it could be more useful to throw an error when the config is invalid instead of clearing. Disabling this option will make it throw a `SyntaxError` on invalid config instead of clearing.
+
+		@default true
+	*/
+	clearInvalidConfig?: boolean;
+	/**
+		Name of the config file (without extension).
+
+		Useful if you need multiple config files for your app or module. For example, different config files between two major versions.
+
+		@default 'config'
+	*/
+	configName?: string;
+
+	/**
+		__You most likely don't need this. Please don't use it unless you really have to.__
+
+		The only use-case I can think of is having the config located in the app directory or on some external storage. Default: System default user [config directory](https://github.com/sindresorhus/env-paths#pathsconfig).
+	*/
+	cwd?: string;
+
+	/**
+		Config used if there are no existing config.
+		** Note: The values in `defaults` will overwrite the `default` key in the `schema` option.
+	*/
+	defaults?: Readonly<object>;
+
+	/**
+		Note that this is __not intended for security purposes__, since the encryption key would be easily found inside a plain-text Node.js app.
+
+		Its main use is for obscurity. If a user looks through the config directory and finds the config file, since it's just a JSON file, they may be tempted to modify it. By providing an encryption key, the file will be obfuscated, which should hopefully deter any users from doing so.
+
+		It also has the added bonus of ensuring the config file's integrity. If the file is changed in any way, the decryption will not work, in which case the store will just reset back to its default state.
+
+		When specified, the store will be encrypted using the [`aes-256-cbc`](https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation) encryption algorithm.
+	*/
+	encryptionKey?: string;
+
+	/**
+		Extension of the config file.
+
+		You would usually not need this, but could be useful if you want to interact with a file with a custom file extension that can be associated with your app. These might be simple save/export/preference files that are intended to be shareable or saved outside of the app.
+
+		@default 'json'
+	*/
+	fileExtension?: string;
+
+	/*
+		_Don't use this feature until [this issue](https://github.com/sindresorhus/conf/issues/92) has been fixed._
+
+		You can use migrations to perform operations to the store whenever a version is changed.
+
+		The `migrations` object should consist of a key-value pair of `'version': handler`. The `version` can also be a [semver range](https://github.com/npm/node-semver#ranges).
+
+		@example
+		```
+		import Conf = require('conf');
+
+		const store = new Conf({
+			migrations: {
+				'0.0.1': store => {
+					store.set('debugPhase', true);
+				},
+				'1.0.0': store => {
+					store.delete('debugPhase');
+					store.set('phase', '1.0.0');
+				},
+				'1.0.2': store => {
+					store.set('phase', '1.0.2');
+				},
+				'>=2.0.0': store => {
+					store.set('phase', '>=2.0.0');
+				}
+			}
+		});
+		```
+	*/
+	migrations?: Migrations;
+
+	/**
+		You only need to specify this if you don't have a package.json file in your project or if it doesn't have a name defined within it.
+
+		Default: The name field in the `package.json` closest to where `conf` is imported.
+	*/
+	projectName?: string;
+
+	/**
+		__You most likely don't need this. Please don't use it unless you really have to.__
+
+		Suffix appended to `projectName` during config file creation to avoid name conflicts with native apps.
+
+		You can pass an empty string to remove the suffix.
+
+		For example, on macOS, the config file will be stored in the `~/Library/Preferences/foo-nodejs` directory, where `foo` is the `projectName`.
+
+		@default 'nodejs'
+	*/
+	projectSuffix?: string;
+
+	/**
+		You only need to specify this if you don't have a package.json file in your project or if it doesn't have a version defined within it.
+
+		Default: The name field in the `package.json` closest to where `conf` is imported.
+	*/
+	projectVersion?: string;
+
+	/**
+		* [JSON Schema](https://json-schema.org) to validate your config data.
+		* Under the hood, the JSON Schema validator [ajv](https://github.com/epoberezkin/ajv) is used to validate your config. We use [JSON Schema draft-07](http://json-schema.org/latest/json-schema-validation.html) and support all [validation keywords](https://github.com/epoberezkin/ajv/blob/master/KEYWORDS.md) and [formats](https://github.com/epoberezkin/ajv#formats)
+		* You should define your schema as an object where each key is the name of your data's property and each value is a JSON schema used to validate that property. See more [here](https://json-schema.org/understanding-json-schema/reference/object.html#properties)
+
+		@example
+		```
+		import Conf = require('conf');
+		const schema = {
+			foo: {
+				type: 'number',
+				maximum: 100,
+				minimum: 1,
+				default: 50
+			},
+			bar: {
+				type: 'string',
+				format: 'url'
+			}
+		};
+		const config = new Conf({schema});
+		console.log(config.get('foo'));
+		//=> 50
+		config.set('foo', '1');
+		// [Error: Config schema violation: `foo` should be number]
+		```
+		** Note: The `default` value will be overwritten by the `defaults` option if set.
+	*/
+	schema?: Schema;
+
+	/**
+		Watch for any changes in the config file and call the callback for `onDidChange` if set. This is useful if there are multiple processes changing the same config file.
+
+		__Currently this option doesn't work on Node.js 8 on macOS.__
+
+		@default false
+	*/
+	watch?: boolean;
+
+	/**
+		Function to serialize the config object to a UTF-8 string when writing the config file.
+
+		You would usually not need this, but it could be useful if you want to use a format other than JSON.
+
+		@default value => JSON.stringify(value, null, '\t')
+	*/
+	serialize: Serializer;
+
+	/**
+		Function to deserialize the config object from a UTF-8 string when reading the config file.
+
+		You would usually not need this, but it could be useful if you want to use a format other than JSON.
+
+		@default JSON.parse
+	*/
+	deserialize?: Deserializer;
+};
