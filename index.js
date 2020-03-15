d2f2906630c99248fe477eb07c908cef73cf3fd4
sbencoding <mcbenighost@gmail.com>
2020-03-15 12:54:52 +0100

    Fix wrong merging with upstream

--- index.js
+++ index.js
@@ -552,9 +552,6 @@ class Conf {
 		value = this._serializeExtraTypes(value);
 
 		this._validate(value);
-		let data = this.serialize(value);
-
-		this._validate(value);
 		this._write(value);
 
 		this.events.emit('change');
