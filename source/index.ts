229b02a3c720c30efc6d9b9575389be8c3b49ac5
Michael Telatynski <7t3chguy@gmail.com>
2023-07-17 11:47:24 +0100

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -251,7 +251,6 @@ export default class Conf<T extends Record<string, any> = Record<string, unknown
 	*/
 	delete<Key extends keyof T>(key: Key): void;
 	// This overload is used for dot-notation access.
-	// We exclude `keyof T` as an incorrect type for the default value should not fall through to this overload.
 	delete<Key extends string>(key: Exclude<Key, keyof T>): void;
 	delete(key: string): void {
 		const {store} = this;
